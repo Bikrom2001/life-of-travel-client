478a4a81df9f857f5df066b7493dfdf5c48386f9
Bikrom-Roy <bikromroy2001@gmail.com>
2022-11-07 23:32:31 +0600

    here secation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,15 +12,11 @@ const Header = () => {
           title='Life of Travel'
           className='inline-flex items-center'
         >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 32 32'
-            fill='currentColor'
-            className='flex-shrink-0 w-8 h-8 rounded-full text-gray-900'
-          >
-            <path d='M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z'></path>
-          </svg>
-          <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8">
+						<path d="M412.284,294.37l-12.5,15.642c-8.354,10.454-50.027,64.208-50.027,95.883,0,36.451,28.049,66.105,62.526,66.105s62.527-29.654,62.527-66.105c0-31.675-41.673-85.429-50.028-95.883Zm0,145.63c-16.832,0-30.526-15.3-30.526-34.105,0-11.662,15.485-37.883,30.531-59.2,15.043,21.3,30.522,47.509,30.522,59.2C442.811,424.7,429.116,440,412.284,440Z"></path>
+						<path d="M122.669,51.492,96.133,124.4,30.092,97.205,17.908,126.8l67.271,27.7L26.9,314.606a48.056,48.056,0,0,0,28.689,61.523l184.719,67.232a48,48,0,0,0,61.523-28.688L397.6,151.56Zm149.1,352.236a16,16,0,0,1-20.508,9.563L66.537,346.059a16,16,0,0,1-9.563-20.507L73.553,280H316.8ZM85.2,248l29.594-81.311,36.333,14.961a32.644,32.644,0,1,0,11.236-29.98l-36.615-15.077,16.046-44.085,214.79,78.177L328,249.219V248Z"></path>
+					</svg>
+          <span className='ml-2 text-xl font-bold tracking-wide text-white uppercase'>
           Life of Travel
           </span>
         </Link>
@@ -30,7 +26,7 @@ const Header = () => {
               to='/home'
               aria-label='Home'
               title='Home'
-              className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+              className='font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400'
             >
               Home
             </Link>
@@ -40,7 +36,7 @@ const Header = () => {
               to='/shop'
               aria-label='Shop'
               title='Shop'
-              className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+              className='font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400'
             >
               Shop
             </Link>
@@ -50,7 +46,7 @@ const Header = () => {
               to='/cart'
               aria-label='Cart'
               title='Cart'
-              className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+              className='font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400'
             >
               <p> Cart</p>
             </Link>
@@ -60,7 +56,7 @@ const Header = () => {
               to='/about'
               aria-label='About Us'
               title='About Us'
-              className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+              className='font-medium tracking-wide text-white transition-colors duration-200 hover:text-deep-purple-accent-400'
             >
               About Us
             </Link>
