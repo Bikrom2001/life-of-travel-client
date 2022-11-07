2f34568098894a8dff15e8ae262f49ab3477e67f
Bikrom-Roy <bikromroy2001@gmail.com>
2022-11-08 00:48:29 +0600

    service components add home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,72 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Carousel from "../Carousel/Carousel";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")` }}>
-          <div className="hero-overlay bg-opacity-60"></div>
-          <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="text-4xl font-bold leading-none sm:text-5xl">Quisquam necessita vel
-                <span className="dark:text-violet-400">laborum doloribus</span>delectus
-              </h1>
-              <p className="px-8 mt-8 mb-4 text-lg">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
-              <div className="flex flex-wrap justify-center">
-                <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Get started</button>
-                <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-white">Learn more</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">❮</a>
-          <a href="#slide2" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://images.pexels.com/photos/1268855/pexels-photo-1268855.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")` }}>
-          <div className="hero-overlay bg-opacity-60"></div>
-          <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="text-4xl font-bold leading-none sm:text-5xl">Quisquam necessita vel
-                <span className="dark:text-violet-400">laborum doloribus</span>delectus
-              </h1>
-              <p className="px-8 mt-8 mb-4 text-lg">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
-              <div className="flex flex-wrap justify-center">
-                <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Get started</button>
-                <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-white">Learn more</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://images.pexels.com/photos/1008155/pexels-photo-1008155.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")` }}>
-          <div className="hero-overlay bg-opacity-60"></div>
-          <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="text-4xl font-bold leading-none sm:text-5xl">Quisquam necessita vel
-                <span className="dark:text-violet-400">laborum doloribus</span>delectus
-              </h1>
-              <p className="px-8 mt-8 mb-4 text-lg">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
-              <div className="flex flex-wrap justify-center">
-                <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Get started</button>
-                <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-white">Learn more</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide1" className="btn btn-circle">❯</a>
-        </div>
-      </div>
+    <div>
+      <Carousel></Carousel>
+      <Services></Services>
+      
     </div>
   );
 };
---
+++ src/components/Services/Services.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <section className="dark:bg-gray-800 dark:text-gray-100">
+            <div className="container p-6 mx-auto space-y-8 sm:space-y-12">
+                <div className="space-y-2 text-center">
+                    <h2 className="text-3xl font-bold">Partem reprimique an pro</h2>
+                    <p className="font-serif text-sm dark:text-gray-400">Qualisque erroribus usu at, duo te agam soluta mucius.</p>
+                </div>
+                <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                    <a rel="noopener noreferrer" href="#" className=" mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
+                        <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?1" />
+                        <div className="p-6 space-y-2">
+                            <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
+                            <span className="text-xs dark:text-gray-400">January 21, 2021</span>
+                            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
+                        </div>
+                    </a>
+                    <a rel="noopener noreferrer" href="#" className=" mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
+                        <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?2" />
+                        <div className="p-6 space-y-2">
+                            <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
+                            <span className="text-xs dark:text-gray-400">January 22, 2021</span>
+                            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
+                        </div>
+                    </a>
+                    <a rel="noopener noreferrer" href="#" className=" mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
+                        <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?3" />
+                        <div className="p-6 space-y-2">
+                            <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
+                            <span className="text-xs dark:text-gray-400">January 23, 2021</span>
+                            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
+                        </div>
+                    </a>
+
+                </div>
+
+            </div>
+        </section>
+    );
+};
+
+export default Services;
